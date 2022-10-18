a1e84d5acd456d65849816cf7b251a1ece36528f
Jirawat-rackz <Jirawat_ku@kkumail.com>
2022-10-18 14:47:05 +0700

    [feat] init new project & navigate controller

--- tutorial-app/ViewController.swift
+++ tutorial-app/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AViewController: UIViewController {
 
+    @IBOutlet weak var buttonB: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("viewDidLoad")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("viewDidDisappear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("viewDidDisappear")
     }
 
-
+    @IBAction func onClickRedirectToB(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let bViewController = storyboard.instantiateViewController(withIdentifier: "bViewController") as! BViewController
+        self.navigationController?.pushViewController(bViewController, animated: true)
+    }
 }
 
